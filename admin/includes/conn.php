20c14b6bc276f7ab0a349bfd3f7b9b9fa2a50538
Shashwat Chouhan <ishashwat@tutanota.com>
2023-04-12 23:08:53 +0530

    first commit

---
+++ admin/includes/conn.php
@@ -0,0 +1,8 @@
+<?php
+	$conn = new mysqli('localhost', 'root', '', 'votesystem','3307');
+
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
+	
+?>
---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Virtual Voting Machine</b>
+    </div>
+    <strong> <a href="https://code-projects.org/">SAAT-7</a></strong>
+</footer>
